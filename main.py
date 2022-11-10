22a59b1009e82cb523ecaf2f6063fda3be5a8a5e
yewon918 <yewon918@naver.com>
2022-11-10 17:49:26 +0900

    11/10

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import flask
 from flask import Flask, request, render_template
 import cv2
 from datetime import datetime
+import os
+import numpy as np
+from PIL import Image
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
 # 메인페이지 - url 요청시 기본 index.html로 이동 (렌더링)
-# @app.route("/", methods=['POST', 'GET'])
 @app.route("/", methods=['POST','GET'])
 def cam_main():
     return render_template('camera.html')
@@ -19,16 +22,105 @@ def cam_main2():
 
 
 @app.route("/update", methods=['POST', 'GET'])    # 변경가능, 임시
-def makemodel():
-    return flask.render_template('camera.html', d0="업데이트를 완료했습니다!")
+def retrain():
+    if request.method == 'POST':
+        file = request.files['image']
+
+        faceCascade = cv2.CascadeClassifier(
+            r"C:\Users\Yewon\anaconda3\envs\py38\Library\etc\haarcascades\haarcascade_frontalface_default.xml")
+
+        file.save("./"+ secure_filename(f.filename))
+        image = cv2.imread(file)  ## file 자체가 아닌 경로를 읽어오기 때문에 오류 발생
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        face_id = input('\n enter user id end press <return> ==> ')
+
+        # console message  - int형으로 입력받음
+        names = []
+
+        count = 0
+        # 영상 처리 및 출력
+        while True:
+            faces = faceCascade.detectMultiScale(  # 얼굴 위치 검출
+                gray,  # 검출하고자 하는 원본이미지
+                scaleFactor=1.2,  # 검색 윈도우 확대 비율, 1보다 커야 한다
+                minNeighbors=6,  # 얼굴 사이 최소 간격(픽셀)
+                minSize=(20, 20)  # 얼굴 최소 크기. 이것보다 작으면 무시
+            )
+
+            # 얼굴에 대해 rectangle 출력
+            for (x, y, w, h) in faces:
+                cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                count += 1
+                cv2.imwrite("./dataset2/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
+
+            # cv2.imshow('image', gray)
+
+            # 종료조건
+            if cv2.waitKey(1) > 0:
+                break
+            elif count >= 100:
+                break  # 데이터셋 만드는 수 지정
+
+        print("\n [INFO] Exiting Program and cleanup stuff")
+
+        cv2.destroyAllWindows()  # 모든 윈도우 창 닫기
+
+        #----
+        path = './dataset2'  # 경로 (dataset 폴더)
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        detector = cv2.CascadeClassifier(
+            r"C:\Users\Yewon\anaconda3\envs\py38\Library\etc\haarcascades\haarcascade_frontalface_default.xml")
+
+        print('\n [INFO] Training faces. It will take a few seconds. Wait ...')
+
+        def getImagesAndLabels(path):
+            detector = cv2.CascadeClassifier(
+                r"C:\Users\Yewon\anaconda3\envs\py38\Library\etc\haarcascades\haarcascade_frontalface_default.xml")
+
+            imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+            # listdir : 해당 디렉토리 내 파일 리스트
+            # path + file Name : 경로 list 만들기
+
+            faceSamples = []
+            ids = []
+            for imagePath in imagePaths:  # 각 파일마다
+                # 흑백 변환
+                PIL_img = Image.open(imagePath).convert('L')  # L : 8 bit pixel, bw
+                img_numpy = np.array(PIL_img, 'uint8')
+
+                # user id
+                id = int(os.path.split(imagePath)[-1].split(".")[1])  # 마지막 index : -1
+
+                # 얼굴 샘플
+                faces = detector.detectMultiScale(img_numpy)
+                for (x, y, w, h) in faces:
+                    faceSamples.append(img_numpy[y:y + h, x:x + w])
+                    ids.append(id)
+
+            return faceSamples, ids
+
+        # ------
+        faces, ids = getImagesAndLabels(path)  # 데이터셋에서 뽑아냄
+        recognizer.train(faces, np.array(ids))  # 학습
+
+        # 모델 저장
+        recognizer.write('./trainer/trainer.pkl')
+        print('\n [INFO] {0} faces trained. Exiting Program'.format(len(np.unique(ids))))
+
+        cv2.destroyAllWindows()
+
+        return flask.render_template('camera.html', d0="업데이트를 완료했습니다!")
+
+
+
 
 # 데이터
 # 데이터 예측 처리
 visit_list=[]
 tmp = []
-@app.route('/predict', methods=['POST', 'GET'])
 
-########################## 카메라 켜서 예측하는 부분 #################################
+@app.route('/predict', methods=['POST', 'GET'])
 def make_prediction():
     if request.method == 'POST':
 
@@ -92,6 +184,9 @@ def make_prediction():
 
 
 if __name__ == '__main__':
+    # model = joblib.load('./trainer/tainer.pkl')
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read('./trainer/trainer.pkl')  # 학습된 모델을 불러옴
     # Flask 서비스 스타트
     app.run(host='0.0.0.0', port=5000, debug=True)
 
